ee99a81997937b71d59acf51d600067a391656cb
Yagya Singh Rajawat <yagya@iboxz.in>
2020-09-06 19:58:29 +0530

    Updates

---
+++ precache-manifest.c444792d66a994ad8b1b4d2ef616a6b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591212178c71f74ca2823579a7f340ee",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "9b03253abff0c02bbd5c",
+    "url": "/tic-tac-toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "71ff86856a830ac57415",
+    "url": "/tic-tac-toe/static/css/main.899ef0ac.chunk.css"
+  },
+  {
+    "revision": "9b03253abff0c02bbd5c",
+    "url": "/tic-tac-toe/static/js/2.85a96c79.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.85a96c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ff86856a830ac57415",
+    "url": "/tic-tac-toe/static/js/main.601da720.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
